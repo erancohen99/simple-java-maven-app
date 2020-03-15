12e82fe3c08d9afa141494dfead63c8cb82a16b6
Eran <eran.cohen@emc.com>
2020-03-15 15:18:55 +0000

    Add bunch of assert testings

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -32,6 +32,29 @@ public class AppTest
     @Test
     public void testAppMain()
     {
+        //Variable declaration		
+        String string1="Junit";					
+        String string2="Junit";					
+        String string3="test";					
+        String string4="test";					
+        String string5=null;					
+        int variable1=1;					
+        int	variable2=2;					
+        int[] airethematicArrary1 = { 1, 2, 3 };					
+        int[] airethematicArrary2 = { 1, 2, 3 };					
+        		
+        //Assert statements		
+        assertEquals(string1,string2);					
+        assertSame(string3, string4);					
+        assertNotSame(string1, string3);					
+        assertNotNull(string1);			
+        assertNull(string5);			
+        assertTrue(variable1<variable2);					
+        assertArrayEquals(airethematicArrary1, airethematicArrary2);					
+
+        		
+        //Assert statements		
+        assertEquals(string1,string2);
 		//Assume.assumeFalse(System.getProperty("os.name").contains("Windows"));
         App.main(null);
         try {
